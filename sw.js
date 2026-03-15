7c4679f9eb4437c6d6233d095be8b54ddd365a8a
Andrew McCracken <an.sc.mccracken@gmail.com>
2026-03-14 19:44:50 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'skin-ai-v1';
+const assets = [
+  './', 
+  './index.html', 
+  './model.json', 
+  './metadata.json', 
+  './weights.bin'
+];
+
+// Install the service worker and cache all files locally
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching medical assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
